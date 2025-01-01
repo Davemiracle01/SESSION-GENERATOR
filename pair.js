f2f63baf874cac19a4bf71d1ea9ad8a836f27955
Davemiracle01 <davidmiracle514@gmail.com>
2025-01-01 07:14:23 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,19 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
-*_Made With 🤍_*
+*ASTA BOT SESSION ID GENERATED SUCCESSFULLY*
+*_powerd by dave_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+║ *remember that once you share your session id your deployer has acsess to your chats 😹*
+║ 
 ╚════════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+║  『••• incase of anything text me   •••
+║❒ *Owner:* _https://wa.me/254114127157_
+║❒ *Repo:* _https://github.com/Davemiracle01/ASTA-BOT_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VavpWUvGk1Fkbzz0vz0v_
+║❒ *Plugins:* _https://github.com/Davemiracle01/ASTA-BOT_
 ╚════════════════════════╝
 _____________________________________
 
